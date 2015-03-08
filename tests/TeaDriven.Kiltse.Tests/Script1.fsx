f352e7045f015db8b38d5de7abd26833c7cf89f6
TeaDrivenDev <TeaDrivenDev@gmail.com>
2015-03-08 02:00:08 +0100

    Extracted test window setup to new script file

--- tests/TeaDriven.Kiltse.Tests/Script1.fsx
+++ tests/TeaDriven.Kiltse.Tests/Script1.fsx
@@ -16,69 +16,10 @@ open TeaDriven.Kiltse
 open System.Windows.Input
 
 
-
-let createEnvironment() =
-    let window = Window(Width = 1000., Height = 800.)
-    let canvas = Canvas()
-    canvas.Background <- Brushes.Black
-    window.Content <- canvas
-    window.Show()
-
-    window, canvas
-
-let window, canvas = createEnvironment()
-window.SizeChanged.AddHandler <| SizeChangedEventHandler (fun sender e ->
-                                    let window = sender :?> Window
-                                    let panel = window.Content :?> Panel
-                                    panel.Height <- window.Height
-                                    panel.Width <- window.Width)
-
-type DragDrop =
-    {
-        mutable MouseCaptured : bool
-        mutable CanvasPosition : float * float
-        mutable ControlPosition : float * float    
-        mutable Source : UIElement
-    }
-
-let dragDrop = { MouseCaptured = false; CanvasPosition = 0., 0.; ControlPosition = 0.,  0.; Source = null }
+#load "TestWindow.fsx"
 
 
-let mouseDown (sender : obj) (e : MouseButtonEventArgs) =
-    dragDrop.Source <- sender :?> UIElement
-    dragDrop.Source |> Mouse.Capture |> ignore
-    dragDrop.MouseCaptured <- true
-    dragDrop.ControlPosition <- Canvas.GetLeft dragDrop.Source, Canvas.GetTop dragDrop.Source
-    dragDrop.CanvasPosition <- e.GetPosition(canvas).X, e.GetPosition(canvas).Y
-
-let mouseMove (sender : obj) (e: MouseEventArgs) =
-    if dragDrop.MouseCaptured then
-        let x, y = e.GetPosition(canvas).X, e.GetPosition(canvas).Y
-        
-        let controlX, controlY = dragDrop.ControlPosition
-        let canvasX, canvasY = dragDrop.CanvasPosition
-        let newControlX, newControlY = controlX + x - canvasX, controlY + y - canvasY
-
-        Canvas.SetLeft(dragDrop.Source, newControlX)
-        Canvas.SetTop(dragDrop.Source, newControlY)
-        
-        dragDrop.ControlPosition <- newControlX, newControlY
-        dragDrop.CanvasPosition <- x, y
-
-let mouseUp (sender : obj) (e : MouseButtonEventArgs) =
-    Mouse.Capture null |> ignore
-    dragDrop.MouseCaptured <- false
-
-
-let makeDraggable (control : UIElement) =
-    control.MouseLeftButtonDown.AddHandler <| MouseButtonEventHandler mouseDown
-    control.MouseMove.AddHandler <| MouseEventHandler mouseMove
-    control.MouseLeftButtonUp.AddHandler <| MouseButtonEventHandler mouseUp
-
-
-
-
-
+let window, canvas = TestWindow.window, TestWindow.canvas
 
 let names =
     [
@@ -171,9 +112,6 @@ let defineParameters windowSize =
 
     name, position, (fun () -> ()), [0..numberOfSatellites-1]
 
-let (||||>) (a, b, c, d) f = f a b c d
-
-
 let createSky numberOfStars =
     [1..numberOfStars]
     |> List.map (fun _ ->
@@ -182,7 +120,7 @@ let createSky numberOfStars =
             let ring = Ring(Radius = radius, DisplayName = name, ItemsSource = (items |> List.map box))
             Canvas.SetLeft(ring, x)
             Canvas.SetTop(ring, y)
-            makeDraggable ring
+            TestWindow.makeDraggable ring
 
             ring))
     |> List.iter (canvas.Children.Add >> ignore)
@@ -199,7 +137,7 @@ createSky 5
 
 
 let ring = Ring(Radius = 250., DisplayName = "Large", ItemsSource = ([0..200] |> List.map box))
-makeDraggable ring
+TestWindow.makeDraggable ring
 Canvas.SetLeft(ring, 400.)
 Canvas.SetTop(ring, 400.)
 
