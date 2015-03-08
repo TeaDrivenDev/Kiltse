ca17b9974e9e8cc154c4148e090fb72cf579488d
TeaDrivenDev <TeaDrivenDev@gmail.com>
2015-03-08 03:49:12 +0100

    Prototype can vary start angle and direction

--- tests/TeaDriven.Kiltse.Tests/Prototype.fsx
+++ tests/TeaDriven.Kiltse.Tests/Prototype.fsx
@@ -16,93 +16,11 @@ let uiColor = Colors.AntiqueWhite
 let maxSegments = 30
 
 
-let topLeftCorner (centerX : float, centerY : float) radius = centerX - radius, centerY - radius
-
-let nthValue initial interval n = initial + (n * interval)
-
-let toCartesian angle = Math.PI * angle / 180.
-
-let angles numberOfPoints =
-    let interval = 360. / (float numberOfPoints)
-
-    let nthAngle n =
-        (nthValue 0. interval (float n)) % 360.
-
-    [0..numberOfPoints - 1]
-    |> List.map nthAngle
-
-//    let relativePeripheralCoordinates radius angle =
-//        let cartesianAngle = toCartesian angle
-//
-//        let relativeX = radius * Math.Cos cartesianAngle |> Math.Round
-//        let relativeY = radius * Math.Sin cartesianAngle |> Math.Round
-//
-//        relativeX, -relativeY
-//
-//    let peripheralCoordinates (centerX, centerY) radius numberOfPoints =
-//        angles numberOfPoints
-//        |> List.map (relativePeripheralCoordinates radius)
-//        |> List.map (fun (x, y) -> centerX + x, centerY + y)
-//
-
-let circumference satelliteRadius distance numOfSatellites =
-    (satelliteRadius + distance) * (float numOfSatellites)
-
-let targetRadius minRadius circ =
-    Math.Max(minRadius, (circ / Math.PI) / 2.)
-
-
-
-let createCircle radius center =
-    let circle = Ellipse(Width = 2. * radius, Height = 2. * radius)
-
-    match center with
-    | Some c -> 
-        let left, top = topLeftCorner c radius
-        Canvas.SetLeft(circle, left)
-        Canvas.SetTop(circle, top)
-    | None -> ()
-    circle.Fill <- Brushes.Transparent
-    circle.Stroke <- SolidColorBrush uiColor
-
-    circle
-
-
 
 let getEffect (color : Color) =
     let effect = DropShadowEffect(ShadowDepth = 0., Color = color, Opacity = 1., BlurRadius = 12.)
     effect.RenderingBias <- RenderingBias.Quality
     effect
-
-
-
-
-
-let random = Random(DateTime.Now.Millisecond)
-    
-let getRandomPosition (width, height) =
-    let getRandomCoordinate max = (max - 200.) * random.NextDouble() + 100.
-
-    getRandomCoordinate width, getRandomCoordinate height
-
-
-let selectName names =
-    (random.Next(List.length names - 1))
-    |> List.nth names
-
-
-
-let getNumberOfSegments () =
-    if random.Next(7) < 6 then random.Next(1, 9) else random.Next(10, maxSegments)
-
-
-//    let defineParameters windowSize =
-//        let name = selectName names
-//        let position = getRandomPosition windowSize
-//        let numberOfSatellites = getNumberOfSatellites()
-//
-//
-//        name, position, numberOfSatellites
 
 
 let withEffect (circle : Ellipse) = circle.Effect <- getEffect(uiColor); circle
@@ -130,26 +48,7 @@ let getLabelLeft radius (label : FrameworkElement) calculatedX =
     if calculatedX >= radius then calculatedX
     else calculatedX - label.ActualWidth
 
-//    let createLabels radius labelTexts =
-//        let numberOfSatellites = labelTexts |> List.length
-//
-//
-//        peripheralCoordinates (radius, radius) (radius + 12.) numberOfSatellites
-//        |> List.zip labelTexts
-//        |> List.map (fun (text, (x, y)) ->
-//                        let label = TextBlock()
-//                        label.Foreground <- Brushes.AntiqueWhite
-//                        label.Text <- text
-//
-////                        canvas.Children.Add label |> ignore
-//                        Canvas.SetLeft(label, x)
-//                        Canvas.SetTop(label, y)
-////                        Canvas.SetLeft(label, getLabelLeft radius label x)
-////                        Canvas.SetTop(label, y - label.ActualHeight / 2.)
-//                        
-//                        label)
-    
-   
+
 [<Measure>] type deg
 [<Measure>] type rad
 
@@ -163,8 +62,17 @@ let relativePeripheralCoordinates radius angle =
     let relativeX = radius * Math.Cos (polarAngle / 1.<rad>)
     let relativeY = radius * Math.Sin (polarAngle / 1.<rad>)
 
-    relativeY, -relativeX
+    relativeX, -relativeY
 
+
+let contentDistance = 30.
+
+type Direction = Clockwise | Counterclockwise
+
+let adjustForDirection direction (startAngle : float<deg>) (angle : float<deg>) =
+    let sign = if direction = Clockwise then -1. else 1.
+
+    startAngle + (sign * angle)
 
 let arc radius totalInt indexInt =
     let total, index = float totalInt, float indexInt
@@ -174,14 +82,39 @@ let arc radius totalInt indexInt =
 
 
     let arcAngle = 360.<deg> / total
-    let arcStartAngle = index * arcAngle + gapHalfAngle
-    let arcEndAngle = (index + 1.) * arcAngle - gapHalfAngle
 
-    let arcStartX, arcStartY = relativePeripheralCoordinates radius arcStartAngle
-    let arcEndX, arcEndY = relativePeripheralCoordinates radius arcEndAngle
+    let direction = Clockwise
+    let startAngle = 45.<deg>
+
+    let arcStartAngle = index * arcAngle + gapHalfAngle |> adjustForDirection direction startAngle
+    let arcEndAngle = (index + 1.) * arcAngle - gapHalfAngle |> adjustForDirection direction startAngle
+
+
+    let contentCenterAngle = (index + 0.5) * arcAngle
+    let ccRelativeX, ccRelativeY = relativePeripheralCoordinates (radius + contentDistance) contentCenterAngle
+
+    let cx, cy = radius + ccRelativeX, radius + ccRelativeY
+    
+    let tx = TextBlock(Text = "Dings")
+    tx.Foreground <- Brushes.AntiqueWhite
+    tx.FontSize <- 12.
+    tx.Background <- SolidColorBrush(Color.FromRgb(0x10uy, 0x10uy, 0x10uy))
+    Canvas.SetLeft(tx, cx) 
+    Canvas.SetTop(tx, cy)
+
+    let arcStartX, arcStartY =
+        if direction = Clockwise then arcStartAngle else arcEndAngle
+        |> relativePeripheralCoordinates radius
+         
+    let arcEndX, arcEndY =
+        if direction = Clockwise then arcEndAngle else arcStartAngle
+        |> relativePeripheralCoordinates radius
+
 
     let sx, sy = radius + arcEndX, radius + arcEndY
     let ex, ey = radius + arcStartX, radius + arcStartY
+
+    
 
 
     let arcSeg = ArcSegment()
@@ -209,19 +142,25 @@ let arc radius totalInt indexInt =
     arcPath.StrokeThickness <- 2.
     arcPath.Effect <- getEffect color
     arcPath.Data <- pathGeometry
-//        arcPath.ClipToBounds <- false
-
-    arcPath
 
 
+    let arcCanvas = Canvas(Width = 2. * radius, Height = 2. * radius)
+    arcCanvas.HorizontalAlignment <- HorizontalAlignment.Stretch
+    arcCanvas.VerticalAlignment <- VerticalAlignment.Stretch
 
-    
+    [
+        arcPath :> UIElement
+//        tx :> UIElement
+    ]
+    |> List.iter (arcCanvas.Children.Add >> ignore)
 
-let createStar name (x, y) play releases =
-    let numberOfSegments = releases |> List.length
-        
-    let radius = 40.
-    
+    arcCanvas
+
+
+
+let createStar name radius (x, y) segments =
+    let numberOfSegments = segments |> List.length
+            
     let starGrid = Grid(Width = 2. * radius, Height = 2. * radius)
     starGrid.HorizontalAlignment <- HorizontalAlignment.Center
     starGrid.VerticalAlignment <- VerticalAlignment.Center
@@ -245,3 +184,7 @@ let createStar name (x, y) play releases =
 
     starGrid
 
+
+
+createStar "Dings" 50. (200., 200.) [0..7]
+|> canvas.Children.Add
